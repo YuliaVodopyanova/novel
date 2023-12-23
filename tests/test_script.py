51f3e05d185e7aae0f612baa721400cae49b6c2f
YuliaVodopyanova <113133883+YuliaVodopyanova@users.noreply.github.com>
2023-12-23 12:53:09 +0300

    Update test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -7,4 +7,9 @@ def test_jumps():
     Tests if there is label for every jump
     Returns:
     """
-  pass
+  with open(SCRIPT_PATH, 'r') as f:
+      content = f.read()
+      n_jumps = content.count('jump')
+      n_labels = content.count('label')
+      assert (n_jumps + 1) == n_labels
+                              
