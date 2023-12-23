430df4b8a7a05630be6aaf48d032879a9242a1aa
YuliaVodopyanova <113133883+YuliaVodopyanova@users.noreply.github.com>
2023-12-23 12:43:05 +0300

    add test for script file

---
+++ tests/test_script.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+SCRIPT_PATH = Path(__file__).parent.parent / 'game' / 'script.rpy'
+
+def test_jumps():
+    """
+    Tests if there is label for every jump
+    Returns:
+    """
+  pass
